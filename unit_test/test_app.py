5b016cdb084cc8acf4f486092cc9896fcc399ec4
foxolave <113529028+foxolave@users.noreply.github.com>
2024-04-17 17:10:24 +0300

    Update test_app.py

--- unit_test/test_app.py
+++ unit_test/test_app.py
@@ -10,7 +10,7 @@ class TestApp(unittest.TestCase):
     def test_get_status(self):
         response = self.app.get('/status')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {'status': 'running_v1'})
+        self.assertEqual(response.json, {'status': 'running_v2'})
 
 if __name__ == '__main__':
     unittest.main()
